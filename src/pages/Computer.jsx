cf2d5ccfb8f9ff0689b7059c3054259c08066c1b
Anna N <nahid.najafi.h@gmail.com>
2024-06-01 00:52:29 +0200

    modified footer

--- src/pages/Computer.jsx
+++ src/pages/Computer.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Computer = () => {
   return (
-    <div>Computer</div>
-  )
+    <div className="w-5/6 mx-auto flex my-4">
+      <div className="flex flex-wrap gap-4 static">
+        <div className="sub-container h-80 w-48 border border-grey-400 hover:bg-gray-100" ></div>
+      </div>
+    </div>
+  );
 }
 
 export default Computer
